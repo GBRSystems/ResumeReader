cc3e3a12db72c20b0fda7a5b1de8a25d77a64188
Gigum Bandara Rajaguru <gigumbrajaguru@gmail.com>
2022-11-05 19:00:11 +0530

    Fixed issues in main

--- handle_request.py
+++ handle_request.py
@@ -11,15 +11,17 @@ from pdfminer.pdfparser import PDFParser
 from pdf2image import convert_from_path
 import pytesseract
 from pytesseract import image_to_string
+
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
 
 
 class EMSIAPIManagement:
     def __init__(self):
+        self.response = None
+        self.token = None
         __CLIENT_ID = "15vr6i8p2mx92c09"
         __CLIENT_SECRET = "VSO1EWcM"
         url = "https://auth.emsicloud.com/connect/token"
-
         payload = f"client_id={__CLIENT_ID}&client_secret={__CLIENT_SECRET}&grant_type=client_credentials&scope=emsi_open"
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
         response = requests.request("POST", url, data=payload, headers=headers)
@@ -106,15 +108,23 @@ class FileManager:
         file_temp = open(
             f"./temp_files/temp_cv_{self.username}.txt", "w+", encoding="utf-8"
         )
-        if text and len(text)>2:
+        if text and len(text) > 2:
             file_temp.write(text)
+            return text
+        elif text:
+            try:
+                images = convert_from_path(self.attached_file_location)
+                final_text = ""
+                for pg, img in enumerate(images):
+                    final_text += image_to_string(img)
+                text = self.__set_clean_text(final_text)
+                file_temp.write(text)
+                return text
+            except Exception as e:
+                print(e)
+                return None
         else:
-            images = convert_from_path(self.attached_file_location)
-            final_text = ""
-            for pg, img in enumerate(images):
-                final_text += image_to_string(img)
-            text = self.__set_clean_text(final_text)
-            file_temp.write(text)
+            return None
 
     def __set_clean_text(self, text=None):
         if text:
@@ -140,8 +150,6 @@ class FileManager:
         text = re.sub("[^A-Za-z0-9@.]+", " ", output_string)
         text = text.replace("\n", "\\n").replace("\t", "\\t")
         return text
-
-
 
 
 class RetrieveSkills:
